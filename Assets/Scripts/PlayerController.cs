80e9e5942ab84a3c7fc399c13241f2d2e51b1997
Mmd <sensimdh81@gmail.com>
2023-07-04 20:35:31 +0330

    bike movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed = 10.0f;
+	public float rotateSpeed = 10.0f;
+	public float resetSpeed = 1.0f;
+	public Transform child;
+	private Rigidbody rb;
+	private Quaternion originalRotation;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		originalRotation = child.rotation;
+	}
+
+	void FixedUpdate()
+	{
+		if (Input.GetMouseButton(0))
+		{
+			float moveHorizontal = Input.GetAxis("Mouse X");
+
+			Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+
+			rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+
+			float rotateHorizontal = Input.GetAxis("Mouse X");
+
+			Vector3 rotation = new Vector3(0.0f, rotateHorizontal * rotateSpeed, 0.0f);
+
+			child.Rotate(rotation * Time.deltaTime);
+		}
+
+		child.rotation = Quaternion.Slerp(child.rotation, originalRotation, resetSpeed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/TilingAnimator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class TilingAnimator : MonoBehaviour
+{
+	public float speed = 1f;
+	public Vector2 direction = Vector2.right;
+
+	private Renderer renderer;
+
+	void Start()
+	{
+		renderer = GetComponent<Renderer>();
+	}
+
+	void Update()
+	{
+		float offset = Time.time * speed;
+		Vector2 offsetVector = direction * offset;
+		renderer.material.mainTextureOffset = offsetVector;
+	}
+}
