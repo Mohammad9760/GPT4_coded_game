4aa05cb566f6ac91c8cb69c4c3c4eb63788251f3
Mmd <sensimdh81@gmail.com>
2023-07-06 18:34:29 +0330

    obstacle object pool spawning

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+	public float minSpeed;
+	public float maxSpeed;
+	public Vector3 direction;
+	private Rigidbody rb;
+	private float speed;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		speed = Random.Range(minSpeed, maxSpeed);
+	}
+
+	void FixedUpdate()
+	{
+		rb.MovePosition(transform.position + direction * speed * Time.fixedDeltaTime);
+	}
+}
+
---
+++ Assets/Scripts/SpawnRandomPrefab.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnRandomPrefab : MonoBehaviour
+{
+	public List<GameObject> prefabs;
+	public List<Transform> spawnPoints;
+	public float minDelay = 1f;
+	public float maxDelay = 3f;
+	public Vector3 rotation = new Vector3(-90f, 0f, 90f);
+	public int poolSize = 10;
+
+	private List<GameObject> objectPool;
+
+	void Start()
+	{
+		objectPool = new List<GameObject>();
+
+		for (int i = 0; i < poolSize; i++)
+		{
+			int randomIndex = Random.Range(0, prefabs.Count);
+			GameObject prefab = prefabs[randomIndex];
+
+			GameObject obj = Instantiate(prefab, Vector3.zero, Quaternion.identity);
+			obj.SetActive(false);
+
+			objectPool.Add(obj);
+		}
+
+		StartCoroutine(SpawnPrefabs());
+	}
+
+	IEnumerator<object> SpawnPrefabs()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(Random.Range(minDelay, maxDelay));
+
+			int randomIndex = Random.Range(0, prefabs.Count);
+			GameObject prefab = prefabs[randomIndex];
+
+			int randomSpawnIndex = Random.Range(0, spawnPoints.Count);
+			Transform spawnPoint = spawnPoints[randomSpawnIndex];
+
+			GameObject obj = GetObjectFromPool();
+			obj.transform.position = spawnPoint.position;
+			obj.transform.rotation = Quaternion.Euler(rotation);
+			obj.SetActive(true);
+
+			StartCoroutine(ReturnObjectToPoolCoroutine(obj));
+		}
+	}
+
+	GameObject GetObjectFromPool()
+	{
+		foreach (GameObject o in objectPool)
+		{
+			if (!o.activeInHierarchy)
+			{
+				return o;
+			}
+		}
+
+		int randomIndex = Random.Range(0, prefabs.Count);
+		GameObject prefab = prefabs[randomIndex];
+
+		GameObject obj = Instantiate(prefab, Vector3.zero, Quaternion.identity);
+		obj.SetActive(false);
+
+		objectPool.Add(obj);
+
+		return obj;
+	}
+
+	IEnumerator<object> ReturnObjectToPoolCoroutine(GameObject o)
+	{
+		yield return new WaitForSeconds(Random.Range(2f, 5f));
+
+		o.SetActive(false);
+	}
+}
